806be0cebae15f575ec1ca3b9f7158319f183bd0
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-01-28 11:32:54 +0100

    bug fixed

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ var apiKey = `9f60d7631c91fefe3d27ab9b78997410`;
 var units = "metric";
 var unit_symbol = "°C";
 var apiRequestCurrent = `https://api.openweathermap.org/data/2.5/weather?`;
-var apiRequestForecast = `https://api.openweathermap.org/data/2.5/forecast/daily?`;
+var apiRequestForecast = `https://api.openweathermap.org/data/2.5/forecast?`;
 var default_city = "london";
